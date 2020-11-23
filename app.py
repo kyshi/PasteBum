8a79f3514563f94f8a3f09bcf025b558640822fc
kyshi <kyshi58@outlook.com>
2020-11-23 21:28:51 +0300

    CORS Update, version 0.4

--- app.py
+++ app.py
@@ -3,8 +3,10 @@ from datetime import datetime
 
 from flask import Flask, redirect, render_template, request, url_for, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///notes.db"
 db = SQLAlchemy(app)
 
