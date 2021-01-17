e667d6c4732aefd46d4cf1671b94f1691205fc3b
kyshi <kyshi58@outlook.com>
2021-01-18 00:52:42 +0300

    Updated API and note system, version 0.8

--- README.md
+++ README.md
@@ -2,19 +2,27 @@
 ![PasteBum](https://i.hizliresim.com/4i3SVl.png)
 
 ![License](https://img.shields.io/badge/license-MIT-green)
-![Version](https://img.shields.io/badge/version-0.7-blue)
+![Version](https://img.shields.io/badge/version-0.8-blue)
 ![Status](https://img.shields.io/badge/status-alpha-red)
 
 ## What is PasteBum?
 [PasteBum](https://pastebum.herokuapp.com/) is a text share service. You can share plain text or code.
 
-## Features (version 0.7 | alpha)
-Big frontend update.<br>
+## Features (version 0.8 | alpha)
+Updated API system.<br>
+Renewed API Documentation.<br>
+Line shift issue resolved.<br>
+Added rate limit.<br>
+Title can't empty.<br>
+Content can't empty. (so... partly.)<br>
+A slight change in Informative Text.<br>
+Small adjustments.<br>
 
 ## Technologies
 - Python/Flask (Main/Backend)
 - Flask SQlAlchemy (ORM Structure)
-- Flask CORS (Cors Error)
+- Flask CORS (Cors)
+- Flask Limiter (Rate Limit)
 - SQLite (Database)
 - HTML/CSS (Frontend)
 - Javascript (Frontend)
--- app.py
+++ app.py
@@ -1,14 +1,24 @@
+import random
+import can_not_empty
 from hashlib import blake2b
 from datetime import datetime
 from flask import Flask, redirect, render_template, request, url_for, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
+
 
 app = Flask(__name__)
 CORS(app)
+limiter = Limiter(
+    app,
+    key_func=get_remote_address,
+    default_limits=["10 per second"]
+)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///notes.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
-
 
 @app.route("/")
 def index():
@@ -20,18 +30,24 @@ def create_note(url):
     if request.method == "POST":
         title = request.form.get("title")
         content = request.form.get("content")
+        if len(content) == 0:
+            content = random.choice(can_not_empty.random_content)
         syntax = request.form.get("syntax")
         now = datetime.now()
         url_seed = f"{str(now)}+{title}+{content}"
         line = ""
-        for i in range(1, content.count("\n") + 2):
-            line += f"{i}\n"
+        if content[-1] == "\n":
+            for i in range(1, content.count("\n") + 1): #? last line is empty
+                line += f"{i}\n"
+        else:
+            for i in range(1, content.count("\n") + 2): #? last line is not empty
+                line += f"{i}\n"
         newNote = Note(
             title=title,
-            content=content,
+            content=content, 
             syntax=syntax,
             date=now,
-            data_size=len(content.replace("\n", ""))+len(title)+1-int(line[-2]),
+            data_size=len(content.replace("\n", "")),
             line=line,
             url=blake2b(url_seed.encode("utf-8"), digest_size=5).hexdigest(),
         )
@@ -51,55 +67,70 @@ def create_note(url):
             line=note.line,
         )
 
+@limiter.limit("10 per hour")
 @app.route("/api/create_note/<string:title>/<string:content>/<string:syntax>")
 def api_crete_note(title, content, syntax):
     title = title
-    content = content
+    content = content.replace("$n*", "\n")
     syntax = syntax
+    all_syntax = ('plain text', 'c', 'cpp', 'csharp', 'css', 'dart', 'go', 'html', 'java', 'javascript', 'python')
+    if syntax not in all_syntax:
+        json_data = {
+        "error": "Invalid syntax."
+        }
+        return jsonify(json_data)
     now = datetime.now()
     url_seed = f"{str(now)}+{title}+{content}"
     line = ""
-    for i in range(1, content.count("\n") + 2):
-        line += f"{i}\n"
+    if content[-1] == "\n":
+        for i in range(1, content.count("\n") + 1): #? last line is empty
+            line += f"{i}\n"
+    else:
+        for i in range(1, content.count("\n") + 2): #? last line is not empty
+            line += f"{i}\n"
 
     newNote = Note(
         title=title,
         content=content,
         syntax=syntax,
         date=now,
-        data_size=len(content)+len(title),
+        data_size=len(content)-int(line[-2])+1,
         line=line,
         url=blake2b(url_seed.encode("utf-8"), digest_size=5).hexdigest(),
     )
     db.session.add(newNote)
     db.session.commit()
-    json_data = [{
+    json_data = {
         "url": blake2b(url_seed.encode("utf-8"), digest_size=5).hexdigest()
-    }]
+    }
     return jsonify(json_data)
 
+@limiter.limit("500 per hour")
 @app.route("/api/view_note/<string:url>")
 def api_view_note(url):
     note = Note.query.filter_by(url=url).first()
-    title = note.title,
-    content = note.content,
-    syntax = note.syntax,
-    date = note.date[:19],
-    data_size = note.data_size,
-    line = note.line,
-    json_data = [{
-        "url": url,
-        "title": title[0],
-        "content": content[0],
-        "syntax": syntax[0],
-        "details":[
-            {
-                "date": date[0],
-                "data_size": data_size[0],
-                "line": line[0]
-            }
-        ]
-    }]
+    if note:
+        title = note.title,
+        content = note.content,
+        syntax = note.syntax,
+        date = note.date[:19],
+        data_size = note.data_size,
+        line = note.line,
+        json_data = {
+            "url": url,
+            "title": title[0],
+            "content": content[0],
+            "syntax": syntax[0],
+            "details":[
+                {
+                    "date": date[0],
+                    "data_size": data_size[0],
+                    "line": line[0]
+                }
+            ]
+        }
+    else:
+        json_data = {"error": "Invalid url."}
     return jsonify(json_data)
 
 @app.route("/api/documentation")
@@ -122,17 +153,20 @@ def informative_text():
 def page_not_found(e):
     return render_template('404.html'), 404
 
+@app.errorhandler(429)
+def too_many_request(e):
+    json_data = {'error': 'Too many request.'}
+    return jsonify(json_data)
+
 class Note(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(50))
     content = db.Column(db.Text)
     syntax = db.Column(db.String(30))
-    date = db.Column(db.String(30)) 
+    date = db.Column(db.String(26)) 
     data_size = db.Column(db.Integer)
     line=db.Column(db.Text)
-    url = db.Column(db.String(20))
+    url = db.Column(db.String(10))
 
-
-if __name__ == "__main__":  
-    db.create_all()
+if __name__ == "__main__":
     app.run()
